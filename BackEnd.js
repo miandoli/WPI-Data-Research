63f2ea0df1c40755d23040180d7ce47c9fa67659
Matt Iandoli <miandoli@gmail.com>
2018-07-16 00:20:52 -0400

    Added a survey and timer after each quiz

--- BackEnd.js
+++ BackEnd.js
@@ -1,3 +1,15 @@
+function randomize() {
+    var lst = ["Ariely_Video.html", "Pinker_Video.html", "Krznaric_Video.html"];
+    var rnd1 = Math.floor(Math.random() * 3);
+    [lst[rnd1], lst[2]] = [lst[2], lst[rnd1]];
+    var rnd2 = Math.floor(Math.random() * 2);
+    [lst[rnd2], lst[1]] = [lst[1], lst[rnd2]];
+    localStorage.setItem("vid1", lst[0]);
+    localStorage.setItem("vid2", lst[1]);
+    localStorage.setItem("vid3", lst[2]);
+    localStorage.setItem("vidIndex", 1);
+}
+
 var myVid;
 function evListener(id) {
     myVid = document.getElementById(id);
@@ -37,27 +49,53 @@ function saveQ(index, numQuestions, nxtPage = 0) {
 }
 
 function vid1() {
+    randomize();
     var name = document.getElementById("txtName").value;
     if (name != "") {
         localStorage.setItem("name", name);
         var time = Date.now() / 1000;
         localStorage.setItem("start", time);
-        window.location = "Ariely_Video.html";
+        window.location = localStorage.getItem("vid1");
     } else {
         window.alert("Please enter a name!");
     }
 }
 
-function vid2() {
-    window.location = "Pinker_Video.html";
+function setTimer() {
+    const length = 0.1;
+    var time = length * 60;
+    var x = setInterval(function() {
+        if (time == 0) {
+            var nxtVid = "vid" + localStorage.getItem("vidIndex");
+            window.location = localStorage.getItem(nxtVid);
+        }
+        var minutes = Math.floor(time / 60);
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+        var seconds = Math.floor(time % 60);
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+        var clock = "" + minutes + ":" + seconds;
+        document.getElementById("timer").innerHTML = clock;
+        time += -1;
+    }, 1000);
 }
 
-function vid3() {
-    window.location = "Krznaric_Video.html";
-}
-
-function endQ() {
+function comfQ() {
+    var index = localStorage.getItem("vidIndex");
+    index++;
+    localStorage.setItem("vidIndex", index);
     window.location = "Comfort_Questions.html";
+}
+
+function determineNxt() {
+    if (localStorage.getItem("vidIndex") <= 3) {
+        window.location = "Timer.html";
+    } else {
+        saveAll(3);
+    }
 }
 
 function multChoice(name, num) {
